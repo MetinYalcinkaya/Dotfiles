e884e3d7e8bd04b2b6ec00e5efdb5792358e3e4f
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2024-11-01 18:46:53 +1100

    Moving nvim config

--- nvim/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-vim.loader.enable()
-require "config/options"
-require "config/keymaps"
-require "config/autocommands"
-require "config/lazy-bootstrap"
-require "config/lazy-plugins"
--- nvim/.config/nvim/lua/plugins/clangd_extensions.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    'p00f/clangd_extensions.nvim',
-    lazy = true,
-    ft = { 'c', 'cpp' },
-    opts = {},
-  },
-}
